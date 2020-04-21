804057624e7e7cde622e73a6074e009480786bea
franco@mumuki.org <franco@mumuki.org>
2020-04-21 18:26:58 +0000

    Mumuki Export on 2020-04-21 18:26:58 +0000

--- extra.js
+++ extra.js
@@ -1,3 +1,48 @@
+/*<hidden-for-student@*/
+// Ver https://github.com/MumukiProject/mumuki-apendice-imperativa-javascript/blob/master/biblioteca.js
+
+// =============
+// Lección 1 y 2
+// =============
+/*@hidden-for-student>*/
+
+// Nos dice cuan largo es un string
+//
+// Por ejemplo:
+//
+//  ム longitud("hola")
+//  4
+function longitud(unString) /*<elipsis-for-student@*/ {
+  return unString.length;
+} /*@elipsis-for-student>*/
+
+// Convierte una palabra en mayúsculas
+//
+// Por ejemplo:
+//
+//  ム convertirEnMayuscula("hola")
+//  "HOLA"
+function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
+  return unString.toUpperCase()
+} /*@elipsis-for-student>*/
+
+// Nos dice si unString empieza con otroString
+//
+// Por ejemplo:
+//
+//  ム comienzaCon("hola todo el mundo", "hola todo")
+//  true
+function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
+  return unString.startsWith(otroString);
+} /*@elipsis-for-student>*/
+
+/*<hidden-for-student@*/
+// =========
+// Lección 3
+// =========
+/*@hidden-for-student>*/
+
+/*<hidden-for-student@*/
 var fakeConsole = {
   lines: [],
   log: function(line) {
@@ -8,22 +53,30 @@ var fakeConsole = {
     return String.prototype.concat.apply([], this.lines);
   }
 };
+/*@hidden-for-student>*/
 
-function longitud(secuencia) {
-  return secuencia.length; 
-}
-function rango(s, e) {
-  let r = [];
-  for (let i = s; i <= e; i++) {
-     r.push(i)
-  }
-  return r;
-}
+// Imprime por pantalla unString
+//
+// Por ejemplo:
+//
+//  ム imprimir("¡estoy imprimiendo!")
+//  true
+function imprimir(unString) /*<elipsis-for-student@*/ {
+  fakeConsole.log(unString)
+} /*@elipsis-for-student>*/
 
-function imprimir(s) {
-  fakeConsole.log(s)
-}
+// Devuelve al azar un número entre 1 y 6
+//
+// Por ejemplo:
+//
+//  ム tirarDado()
+//  5
+//  ム tirarDado()
+//  1
+//  ム tirarDado()
+//  2
+function tirarDado() /*<elipsis-for-student@*/ {
+  return Math.floor(Math.random() * 6) + 1
+} /*@elipsis-for-student>*/
 
-function poner(a, e) {
-   array.push(e);
-}
+
