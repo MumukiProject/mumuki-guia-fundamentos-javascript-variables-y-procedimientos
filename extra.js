687e1a41d525977fbbe605dc17f5b46ec4be3d81
gustavo@mumuki.org <gustavo@mumuki.org>
2020-08-04 21:33:57 +0000

    Mumuki Export on 2020-08-04 21:33:57 +0000

--- extra.js
+++ extra.js
@@ -6,66 +6,87 @@
 // =============
 /*@hidden-for-student>*/
 
-// Nos dice cuan largo es un string
+function longitud(unString) /*<elipsis-for-student@*/ {
+  return unString.length;
+} /*@elipsis-for-student>*/
+// Retorna cuan largo es un string
 //
 // Por ejemplo:
 //
 //  ム longitud("hola")
 //  4
-function longitud(unString) /*<elipsis-for-student@*/ {
-  return unString.length;
-} /*@elipsis-for-student>*/
 
+//==============================================================
+
+function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
+  return unString.toUpperCase()
+} /*@elipsis-for-student>*/
 // Convierte una palabra en mayúsculas
 //
 // Por ejemplo:
 //
 //  ム convertirEnMayuscula("hola")
 //  "HOLA"
-function convertirEnMayuscula(unString) /*<elipsis-for-student@*/ {
-  return unString.toUpperCase()
-} /*@elipsis-for-student>*/
 
-// Nos dice si unString empieza con otroString
+//==============================================================
+
+function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
+  return unString.startsWith(otroString);
+} /*@elipsis-for-student>*/
+// Retorna un booleano que nos dice si unString empieza con otroString
 //
 // Por ejemplo:
 //
 //  ム comienzaCon("hola todo el mundo", "hola todo")
 //  true
-function comienzaCon(unString, otroString) /*<elipsis-for-student@*/ {
-  return unString.startsWith(otroString);
+
+//==============================================================
+
+/*<hidden-for-student@*/
+// ==============
+// Lección 3 y 4
+// ==============
+/*@hidden-for-student>*/
+
+/*<hidden-for-student@*/
+var fakeConsole;
+
+function setUpFakeConsole() {
+  fakeConsole = {
+    lines: [],
+    log: function(line) {
+      this.lines.push(line + "\n");
+      console.log(line);
+    },
+    toString: function() {
+      return String.prototype.concat.apply([], this.lines);
+    }
+  };
+}
+
+function setUpRealConsole() {
+  fakeConsole = console;
+}
+
+setUpFakeConsole();
+/*@hidden-for-student>*/
+
+function imprimir(unString) /*<elipsis-for-student@*/ {
+  fakeConsole.log(unString)
 } /*@elipsis-for-student>*/
-
-/*<hidden-for-student@*/
-// =========
-// Lección 3
-// =========
-/*@hidden-for-student>*/
-
-/*<hidden-for-student@*/
-var fakeConsole = {
-  lines: [],
-  log: function(line) {
-    this.lines.push(line + "\n");
-    console.log(line);
-  },
-  toString: function() {
-    return String.prototype.concat.apply([], this.lines);
-  }
-};
-/*@hidden-for-student>*/
-
 // Imprime por pantalla unString
 //
 // Por ejemplo:
 //
 //  ム imprimir("¡estoy imprimiendo!")
-//  true
-function imprimir(unString) /*<elipsis-for-student@*/ {
-  fakeConsole.log(unString)
-} /*@elipsis-for-student>*/
+//  ¡estoy imprimiendo!
 
-// Devuelve al azar un número entre 1 y 6
+//==============================================================
+
+function tirarDado() /*<elipsis-for-student@*/ {
+  return Math.floor(Math.random() * 6) + 1
+} /*@elipsis-for-student>*/
+// Retorna un número al azar entre 1 y 6
 //
 // Por ejemplo:
 //
@@ -75,8 +96,3 @@ function imprimir(unString) /*<elipsis-for-student@*/ {
 //  1
 //  ム tirarDado()
 //  2
-function tirarDado() /*<elipsis-for-student@*/ {
-  return Math.floor(Math.random() * 6) + 1
-} /*@elipsis-for-student>*/
-
-
